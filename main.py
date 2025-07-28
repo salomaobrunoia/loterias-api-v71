232bce09026138fefa4b54d57b9385ff49b07ed2
salomaobrunoia <salomaobruno@gmail.com>
2025-07-28 02:56:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-
 from fastapi import FastAPI
 from datetime import datetime
 import pytz
-import ntplib
 
 app = FastAPI()
 
 @app.get("/horario")
-def get_horario():
-    try:
-        cliente = ntplib.NTPClient()
-        resposta = cliente.request('a.st1.ntp.br')
-        data_hora = datetime.fromtimestamp(resposta.tx_time, pytz.timezone('America/Sao_Paulo'))
-    except:
-        # fallback para o horário do servidor caso o NTP falhe
-        data_hora = datetime.now(pytz.timezone('America/Sao_Paulo'))
-
+def obter_horario():
+    fuso = pytz.timezone("America/Sao_Paulo")
+    agora = datetime.now(fuso)
     return {
-        "data": data_hora.strftime("%Y-%m-%d"),
-        "hora": data_hora.strftime("%H:%M:%S"),
-        "dia_da_semana": data_hora.strftime("%A").lower(),
-        "fuso": "America/Sao_Paulo",
-        "origem": "NTP @ a.ntp.br"
+        "data": agora.strftime("%Y-%m-%d"),
+        "hora": agora.strftime("%H:%M:%S"),
+        "fuso": "UTC-3"
     }
